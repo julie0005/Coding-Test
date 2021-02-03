225e8d015784ccfbf94ffc0a2f6b5d0145a0a792
julie0005 <duliesoures@gmail.com>
2021-02-03 17:27:31 +0900

    Counting Sort

---
+++ Week2/10989.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+//Counting Sort
+int main(){
+
+    int n;
+    int index;
+    int max=0;
+    scanf("%d",&n);
+    int list[10000000];
+    for(int i=1; i<=n; i++){
+        list[i]=0;
+    }
+    for(int i=1; i<=n; i++){
+        scanf("%d",&index);
+        list[index]++;
+        if(index>max){
+            max=index;
+        }
+    }
+    for(int i=1; i<=max; i++){
+        for(int j=0; j<list[i];j++){
+            printf("%d\n",i);
+
+        }
+    }
+
+    return 0;
+}
