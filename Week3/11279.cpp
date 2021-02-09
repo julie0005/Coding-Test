450fe31f7eb49385b7e3c1efb2460d583b5037fe
julie0005 <duliesoures@gmail.com>
2021-02-09 15:23:14 +0900

    priority queue max heap

---
+++ Week3/11279.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <queue>
+#include <functional>
+
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cout.tie(NULL);
+    cin.tie(NULL);
+    priority_queue<int, vector<int>, less<int>> pq;
+    int num,x;
+    cin>>num;
+    for(int i=0; i<num; i++){
+
+        cin>>x;
+        if(x>=1){
+            pq.push(x);
+        }
+        else{
+            if(!pq.empty()){
+                cout<<pq.top()<<'\n';
+                pq.pop();
+            }
+            else{
+                cout<<0<<'\n';
+            }
+        }
+
+    }
+    return 0;
+}
