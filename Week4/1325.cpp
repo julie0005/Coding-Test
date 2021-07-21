371a52e02d9a1c2635684fec2c62aa364d4adc3f
julie0005 <julie0005@ajou.ac.kr>
2021-07-22 03:29:41 +0900

    효율적인 해킹(dfs)

--- Week4/1325.cpp
+++ Week4/1325.cpp
@@ -1,29 +1,35 @@
 /*
 1325 효율적인 해킹
 dfs/bfs 문제
+삽질 기록
 1. 인접 행렬로 푸니 메모리 초과 256mb인데 [10000][10000]은 문제가 됨. => 인접리스트로 풀어야.
+2. 마지막 최댓값 구하는 부분에서 벡터 리스트를 모두 순회해야만 최댓값을 알 수 있는데 업데이트될 때마다 프린트하게 냅둠.
+(max값 오름차순으로 프린트해야한다는 것에 집중하다가 다 순회하고 해야하는데, 첫 순회에서 그래버림.)
+3. A가 B를 신뢰하면, A와 B 모두 해킹할 수 있다는 것을 최대 깊이로 오해함.
 */
 
 #include <iostream>
+#include <vector>
 using namespace std;
 typedef struct{
-    int maxHackCount=0;
+    int maxHackCount=1;
     int connected=0;
     bool visited=false;
 }computers;
 
 int node, edge;
 int start=1;
-int graph[10001][10001];
+vector<int> graph[10001];
 computers computer[10001];
 
 void dfs(int curNode){
     if(!computer[curNode].connected) return;
-    computer[start].maxHackCount++;
-    for(int i=1; i<=node ;i++){
-        if(graph[curNode][i] && !computer[i].visited){
-            computer[i].visited=true;
-            dfs(i);
+    for(int v : graph[curNode]){
+       
+        if(!computer[v].visited){
+            computer[start].maxHackCount++;
+            computer[v].visited=true;
+            dfs(v);
         }
     }
 }
@@ -35,24 +41,28 @@ int main(){
     for(int i=0; i<edge; i++){
         int hack_target, hack_start;
         cin >> hack_target >> hack_start;
-        graph[hack_start][hack_target]=1;
+        graph[hack_start].push_back(hack_target);
         computer[hack_start].connected++;
     }
+    
     for(int i=1; i<=node; i++){
-        start=i;
         for(int j=1; j<=node; j++){
             computer[j].visited=false;
         }
+        start=i;
+        computer[start].visited=true;
         dfs(start);
     }
     int max=0;
-    int maxi=1;
     for(int i=1; i<=node; i++){
         if(max<=computer[i].maxHackCount){
-            maxi=i;
             max=computer[i].maxHackCount;
-            cout<<maxi<<" ";
         }
     }
-    
+    for(int i=1; i<=node; i++){
+        if(max==computer[i].maxHackCount){
+            cout<<i<<" ";
+        }
+    }
+
 }
