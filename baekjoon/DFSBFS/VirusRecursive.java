3c331383066e62500b91f23093c5cd6a808f763f
julie0005 <julie0005@ajou.ac.kr>
2022-01-30 00:43:28 +0900

    2606 바이러스 dfs 재귀함수

---
+++ baekjoon/DFSBFS/VirusRecursive.java
@@ -0,0 +1,41 @@
+package baekjoon.DFSBFS;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+import java.util.StringTokenizer;
+
+public class VirusRecursive {
+    public static boolean[] visited=new boolean[105];
+    public static int[][] graph=new int[101][101];
+    public static int num;
+    public static int infected=0;
+
+    public static void dfs(int computer){
+        visited[computer]=true;
+        for(int i=1; i<=num; i++){
+            if(!visited[i] && graph[computer][i]==1){
+                infected++;
+                dfs(i);
+            }
+        }
+    }
+    public static void main(String[] args) throws IOException{
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        num=Integer.parseInt(br.readLine());
+        int edge=Integer.parseInt(br.readLine());
+        StringTokenizer st=null;
+        
+        for(int i=0; i<edge; i++){
+            st=new StringTokenizer(br.readLine(), " ");
+            int row=Integer.parseInt(st.nextToken());
+            int col=Integer.parseInt(st.nextToken());
+            graph[row][col]=1;
+            graph[col][row]=1;
+        }
+        dfs(1);
+        System.out.println(infected);
+    }
+    
+}
