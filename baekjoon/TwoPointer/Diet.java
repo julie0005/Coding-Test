69179f81e2babd7593a317c8d84706ca87aa3a2c
julie0005 <julie0005@ajou.ac.kr>
2022-02-04 17:05:33 +0900

    1484 다이어트 : 투포인터

---
+++ baekjoon/TwoPointer/Diet.java
@@ -0,0 +1,28 @@
+package baekjoon.TwoPointer;
+/*1484 다이어트
+g : 현재^2- 기억^2 = n^2-r^2 = (n-r)(n+r) = end^2-start^2
+*/
+import java.io.*;
+import java.util.*;
+
+public class Diet {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        int g=Integer.parseInt(br.readLine());
+        int[] seq=new int[g+1];
+        for(int i=0; i<g; i++){
+            seq[i]=i+1;
+        }
+        int start=1, end=1, res=0;
+        boolean isPossible=false;
+        while(start<=g){
+            if(res>=g || end==g-1){
+                start++;
+            }
+            else end++;
+            res=(end-start)*(end+start);
+            if(res==g){System.out.println(end); isPossible=true;}
+        }
+        if(!isPossible) System.out.println(-1);
+    }
+}
