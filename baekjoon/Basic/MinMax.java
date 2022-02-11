d0caeee87fac5942e49473b8c3f68658d0ab2845
julie0005 <julie0005@ajou.ac.kr>
2022-02-11 17:58:41 +0900

    10818 최소 최대 java

---
+++ baekjoon/Basic/MinMax.java
@@ -0,0 +1,21 @@
+package baekjoon.Basic;
+import java.io.*;
+import java.util.*;
+
+public class MinMax {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st=new StringTokenizer(br.readLine());
+        int N=Integer.parseInt(st.nextToken());
+        PriorityQueue<Integer> minHeap=new PriorityQueue<Integer>();
+        PriorityQueue<Integer> maxHeap=new PriorityQueue<Integer>(Collections.reverseOrder());
+        st=new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++){
+            int cur=Integer.parseInt(st.nextToken());
+            maxHeap.offer(cur);
+            minHeap.offer(cur);
+        }
+        System.out.print(minHeap.peek()+" "+maxHeap.peek());
+        
+    }
+}
