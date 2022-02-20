5a9e145df8978950a61e958cfdbe35f05df9bf0b
julie0005 <julie0005@ajou.ac.kr>
2022-02-20 21:37:08 +0900

    CL 숫자 게임

---
+++ programmers/simulation/NumberGame.java
@@ -0,0 +1,22 @@
+package programmers.simulation;
+import java.util.*;
+public class NumberGame {
+    public int solution(int[] A, int[] B) {
+        int answer = 0;
+        Arrays.sort(A);
+        Arrays.sort(B);
+        int aP=A.length-1; //A Pointer
+        int bP=B.length-1; //B Pointer
+        while(aP>=0 && bP>=0){
+            if(B[bP]>A[aP]){
+                answer++;
+                bP--;
+                aP--;
+            }
+            else{
+                aP--;
+            }
+        }
+        return answer;
+    }
+}
